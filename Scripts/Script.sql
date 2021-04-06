32d608ed8e7f96ca9b2289d0fc8bbaecdde76389
SurinJeon <surin0901@naver.com>
2021-04-06 12:55:51 +0900

    studying CSS Layout

---
+++ Scripts/Script.sql
@@ -0,0 +1,15 @@
+select user(), database();
+
+create table member(
+	id varchar(15) not null,
+	passwd char(41),
+	name varchar(15),
+	age int(11),
+	gender varchar(5),
+	email varchar(30),
+	primary key(id)
+);
+
+desc member;
+
+	
